b6fb4e2d46a0997385ab94797f72067b0ba472b7
Şeval Özakça <sevalozakca@gmail.com>
2024-03-10 19:46:57 +0300

    rehber örneği yapıldı (veri çekme)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import { useEffect, useState } from "react";
+import Kisi from "./bilesenler/Kisi";
+import KisiSkelaton from "./bilesenler/KisiSkelaton";
 
 function App() {
+  const [veri, veriGuncelle] = useState([])
   const [yukleniyor, yukleniyorGuncelle] = useState(true)
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
+  useEffect( ()=>{
+    const LSVeriCek = async ()=> localStorage.getItem("rehberJSON")
 
-    async function fetchData() {
-      const response = await fetch('https://jsonplaceholder.org/users');
-      const jsObjectData = await response.json();
-      setData(jsObjectData);
+    async function rehberCek() {
+      const rehberJSON = await LSVeriCek()
+      const rehverVeri = JSON.parse(rehberJSON)
+
+      veriGuncelle(rehverVeri)
       yukleniyorGuncelle(false)
-    };
+    }
 
-    setTimeout( fetchData , 2000 )
+    setTimeout( rehberCek, 2000 )
+  }, [] )
 
-  }, []); 
-
-  console.log(data);
+  console.log(veri);
 
   return (
     <>
-      { yukleniyor && <p>Yükleniyor..</p> }
-      {  data.map(  kisi => <p key={kisi.id}> {kisi.firstname} </p> )  }
+      <section className="container mt-5">
+        <h1>Rehber</h1>
+
+        <div className="row">
+          { yukleniyor && <KisiSkelaton /> }
+          { veri.map( eleman => <Kisi key={eleman.id} ad={eleman.ad} tel={eleman.tel} />  ) }
+        </div>
+
+      </section>
     </>
   );
 }
---
+++ src/bilesenler/KisiSkelaton.jsx
@@ -0,0 +1,61 @@
+function KisiSkelaton() {
+    return (
+      <>
+        <div className="col-lg-3">
+          <div class="card" aria-hidden="true">
+            <div class="card-body">
+              <h2 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-6"></span>
+              </h2>
+              <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div class="card" aria-hidden="true">
+            <div class="card-body">
+              <h2 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-6"></span>
+              </h2>
+              <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div class="card" aria-hidden="true">
+            <div class="card-body">
+              <h2 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-6"></span>
+              </h2>
+              <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div class="card" aria-hidden="true">
+            <div class="card-body">
+              <h2 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-6"></span>
+              </h2>
+              <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default KisiSkelaton;
+  
